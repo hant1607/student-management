01acc3cc458b90d0e8fa440e3d94c782fee8fbe6
hant98 <nguyenha98nq@gmail.com>
2019-08-12 17:05:32 +0700

    login socilal acc

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialAuthController extends Controller
+{
+    public function redirectToProvider($social)
+    {
+        return Socialite::driver($social)->redirect();
+    }
+
+    /**
+     * Obtain the user information from facebook.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    private function findOrCreateUser($social)
+    {
+        $authUser = User::where('provider_id', $social->id)->first();
+
+        if ($authUser) {
+            return $authUser;
+        }
+
+        if ($social->email) {
+            $authUser = User::where('email', $social->email)->first();
+
+            if ($authUser) {
+                if ($authUser->provider_id != $social->id) {
+                    $authUser->update(['provider_id' => $social->id, 'provider' => $social->provider]);
+                }
+                return $authUser;
+            }
+        }
+
+        if (empty($social->email)) {
+            $social->email = sprintf('%s@gmail.com', $social->id);
+        }
+
+        return User::create([
+            'username' => $social->name,
+            'password' => $social->id,
+            'email' => $social->email,
+            'level' => '0',
+            'provider_id' => $social->id,
+            'provider' => $social->provider,
+        ]);
+    }
+
+    public function handleProviderCallback($social)
+    {
+        $user = Socialite::driver($social)->user();
+        $user->provider = $social;
+        $authUser = $this->findOrCreateUser($user);
+        Auth::login($authUser, true);
+
+        return redirect()->route('students.index');
+    }
+}
