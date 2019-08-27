f19450bc1d790934a5f3857f364e394ecbb9033a
hant98 <nguyenha98nq@gmail.com>
2019-08-27 16:32:06 +0700

    faker

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Student;
+use App\Models\ClassModel;
+use Faker\Generator as Faker;
+$faker = \Faker\Factory::create('vi_VN');
+
+$class = ClassModel::select('id')->get();
+
+$factory->define(ClassModel::class, function (Faker $faker){
+    return [
+      'name' => $faker->name,
+    ];
+});
+
+$factory->define(Student::class, function () use ($faker, $class){
+    return [
+        'name' => $faker->name,
+        'user_id' => 1,
+        'class_id' => $class->shuffle()->first()->id,
+        'birthday'=>$faker->date('Y-m-d', 'now'),
+        'gender'=> rand(1,2),
+        'phone'=>$faker->phoneNumber,
+//        'image'=>$faker->image(public_path('upload/'), 100, 70)
+    ];
+});
---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\Models\Student;
+use \App\Models\ClassModel;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        factory(ClassModel::class, 1)->create()->each(function ($class) {
+//            $class->student()->saveMany(
+//              factory(Student::class, 1)->create()
+//            );
+//        });
+        factory(Student::class, 1000)->create();
+    }
+}
